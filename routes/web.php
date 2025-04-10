1f01663921a05be50be7467f0d0b8ad8b6a77b3c
sudipsurve <sudip.surve09@gmail.com>
2025-04-10 17:27:49 +0530

    test

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
